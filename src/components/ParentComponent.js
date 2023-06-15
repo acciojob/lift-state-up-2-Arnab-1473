077da94dd7c447cad452ad4fdd6dc2d4e459a3bc
Arnab-1473 <arban.nilr.1473@gmail.com>
2023-06-15 18:10:44 +0530

    first commit

---
+++ src/components/ParentComponent.js
@@ -0,0 +1,30 @@
+import React, {useState} from "react";
+
+const ParentComponent = () => {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    }
+
+    return(
+        <div>
+            <h1>Parent Component</h1>
+            <p>{inputValue}</p>
+            <ChildComponent inputValue={inputValue} 
+            onInputChange={handleInputChange}/>
+            
+        </div>
+    );
+};
+
+const ChildComponent = ({inputValue, onInputChange}) => {
+    return(
+        <div>
+            <h2>Child Component</h2>
+            <input type="text" value={inputValue} 
+            onChange={onInputChange}/>
+        </div>
+    )
+}
+export default ParentComponent;
